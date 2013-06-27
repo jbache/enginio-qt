e4f9601baaa7e81b7441ac1afd8441eaf4f0f47d
Jens Bache-Wiig <jens.bache-wiig@digia.com>
2013-06-27 20:05:11 +0200

    Redesign of the qml todos example
    - Completely remade graphics
    - Removed controls dependency
    - Added animations

--- examples/qml/config.js
+++ examples/qml/config.js
@@ -1,4 +1,4 @@
 var backendData = {
-    id: "REPLACE_WITH_YOUR_BACKEND_ID",
-    secret: "REPLACE_WITH_YOUR_BACKEND_SECRET",
+    id: "51c86883e5bde50699019ddb",
+    secret: "12eb5c4245caddad9e04160fcd966de0"
 }
